8ec044a77ee1cc970b8b04a046acac9df2476f6c
sjh0824 <1204563557@qq.com>
2018-09-28 17:54:26 +0800

    初始化

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -16,11 +16,29 @@
         </block>
       </swiper>
     </div>
+    <div class="swiperReward">
+      <swiper class="swiper-container" autoplay="true" interval="3000" circular="true" vertical="true" duration="500">
+        <block v-for="(item, index) in rewardText " :key="index">
+          <swiper-item class="swiper-item">
+            <image src="https://yanxuan.nosdn.127.net/31da695c84cabd0eaff054265da29e5c.jpg" class="slide-image" />
+            <span> {{item}}</span>
+          </swiper-item>
+        </block>
+      </swiper>
+    </div>
     <div class="channel">
-      <div @click="categoryList(item.id)" v-for="(item, index) in channel" :key="index">
-        <img :src="item.icon_url" alt="">
-        <p>{{item.name}}</p>
-      </div>
+      <!--<div @click="categoryList(item.id)" v-for="(item, index) in channel" :key="index">-->
+        <!--<img :src="item.icon_url" alt="">-->
+        <!--<p>{{item.name}}</p>-->
+      <!--</div>-->
+      <ul>
+        <scroll-view class="scroll-view" :scroll-x="true">
+          <li @click="goodsDetail(item.id)" v-for="(item, index) in rewardText" :key="index">
+            <span class="rewardTextSpan" :class="{changeColor:item.flag===true}">{{item}}</span>
+            <span class="spanLine"  :class="{changeHideShow:item.flag===true}" ></span>
+          </li>
+        </scroll-view>
+      </ul>
     </div>
     <div class="brand">
       <div @click="tobrandList" class="head">
@@ -45,7 +63,7 @@
       </div>
       <div class="list">
         <ul>
-          <scroll-view class="scroll-view" :scroll-x="true">
+          <scroll-view class="scroll-view" scroll-x>
             <li @click="goodsDetail(item.id)" v-for="(item, index) in newGoods" :key="index">
               <img :src="item.list_pic_url" alt="">
               <p>{{item.name}}</p>
@@ -150,7 +168,8 @@ export default {
       newGoods: [],
       hotGoods: [],
       topicList: [],
-      newCategoryList: []
+      newCategoryList: [],
+      rewardText:["北京的XXX获得50元返利1","北京的XXX获得50元返利2","北京的XXX获得50元返利3","北京的XXX获得50元返利4","北京的XXX获得50元返利5"]
     };
   },
   components: {},
