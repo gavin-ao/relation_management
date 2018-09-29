e71b20f48dc8bf107dccb00950b8f12ad4dd0595
sjh0824 <1204563557@qq.com>
2018-09-29 12:23:29 +0800

    商品详情页

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,13 +1,5 @@
 <template>
   <div class="index">
-    <!--<div class="search">-->
-      <!--<div @click="toMappage">{{cityName}}</div>-->
-      <!--<div @click="toSearch">-->
-        <!--<input type="text" placeholder="搜索商品">-->
-        <!--<span class="icon"></span>-->
-      <!--</div>-->
-    <!--</div>-->
-
     <div class="swiper">
       <swiper class="swiper-container" indicator-dots="true" autoplay="true" interval="3000" circular="true" duration="500">
         <block v-for="(item, index) in banner " :key="index">
@@ -28,112 +20,22 @@
       </swiper>
     </div>
     <div class="channel">
-      <!--<div @click="categoryList(item.id)" v-for="(item, index) in channel" :key="index">-->
-        <!--<img :src="item.icon_url" alt="">-->
-        <!--<p>{{item.name}}</p>-->
-      <!--</div>-->
       <ul>
         <scroll-view class="scroll-view" :scroll-x="true">
-          <li @click="goodsDetail(item.id)" v-for="(item, index) in rewardText" :key="index">
-            <span class="rewardTextSpan" :class="{changeColor:item.flag===true}">{{item}}</span>
+          <li @click="categoryList(item,index)" v-for="(item, index) in channel" :key="index">
+            <span class="rewardTextSpan" :class="{changeColor:item.flag===true}">{{item.name}}</span>
             <span class="spanLine"  :class="{changeHideShow:item.flag===true}" ></span>
           </li>
         </scroll-view>
       </ul>
     </div>
-    <!--<div class="brand">-->
-      <!--<div @click="tobrandList" class="head">-->
-        <!--品牌制造商直供-->
-      <!--</div>-->
-      <!--<div class="content">-->
-        <!--<div @click="branddetail(item.id)" v-for="(item, index) in brandList" :key="index">-->
-          <!--<div>-->
-            <!--<p>{{item.name}}</p>-->
-            <!--<p>{{item.floor_price}}元起</p>-->
-          <!--</div>-->
-          <!--<img :src="item.new_pic_url" alt="">-->
-        <!--</div>-->
-      <!--</div>-->
-    <!--</div>-->
-    <!--<div class="newgoods">-->
-      <!--<div @click="goodsList('new')" class="newgoods-top">-->
-        <!--<div class="top">-->
-          <!--<p>新品首发</p>-->
-          <!--<p>查看全部</p>-->
-        <!--</div>-->
-      <!--</div>-->
-      <!--<div class="list">-->
-        <!--<ul>-->
-          <!--<scroll-view class="scroll-view" scroll-x>-->
-            <!--<li @click="goodsDetail(item.id)" v-for="(item, index) in newGoods" :key="index">-->
-              <!--<img :src="item.list_pic_url" alt="">-->
-              <!--<p>{{item.name}}</p>-->
-              <!--<p>{{item.goods_brief}}</p>-->
-              <!--<p>￥{{item.retail_price}}</p>-->
-            <!--</li>-->
-          <!--</scroll-view>-->
-        <!--</ul>-->
-      <!--</div>-->
-    <!--</div>-->
-    <!--<div class="newgoods hotgoods">-->
-      <!--<div @click="goodsList('hot')" class="newgoods-top">-->
-        <!--<div class="top">-->
-          <!--<p>人气推荐-->
-            <!--<span></span> 好物精选</p>-->
-          <!--<p>查看全部</p>-->
-        <!--</div>-->
-      <!--</div>-->
-      <!--<div class="list">-->
-        <!--<ul>-->
-          <!--<scroll-view class="scroll-view" :scroll-x="true">-->
-            <!--<li @click="goodsDetail(item.id)" v-for="(item, index) in hotGoods" :key="index">-->
-              <!--<img :src="item.list_pic_url" alt="">-->
-              <!--<p>{{item.name}}</p>-->
-              <!--<p>{{item.goods_brief}}</p>-->
-              <!--<p>￥{{item.retail_price}}</p>-->
-            <!--</li>-->
-          <!--</scroll-view>-->
-        <!--</ul>-->
-      <!--</div>-->
-    <!--</div>-->
-    <!--<div class="topicList">-->
-      <!--<div @click="totopic" class="topicList-top">-->
-        <!--专题精选-->
-        <!--<span class="icon"></span>-->
-      <!--</div>-->
-      <!--<div class="list">-->
-        <!--<ul>-->
-          <!--<scroll-view class="scroll-view" :scroll-x="true">-->
-            <!--<li @click="topicdetail(item.id)" v-for="(item, index) in topicList" :key="index">-->
-              <!--<img :src="item.item_pic_url" alt="">-->
-              <!--<div class="btom">-->
-                <!--<div>-->
-                  <!--<p>{{item.title}}</p>-->
-                  <!--<p>{{item.subtitle}}</p>-->
-                <!--</div>-->
-                <!--<div>-->
-                  <!--{{item.price_info}}元起-->
-                <!--</div>-->
-              <!--</div>-->
-            <!--</li>-->
-          <!--</scroll-view>-->
-        <!--</ul>-->
-      <!--</div>-->
-    <!--</div>-->
     <div class="newcategory">
-      <div class="list" v-for="(item, index) in newCategoryList" :key="index">
-        <!--<div class="head">{{item.name}}好物</div>-->
+      <div class="list" v-for="(subitem, subindex) in newCategoryList" :key="subindex">
         <div class="sublist">
-          <div @click="goodsDetail(subitem.id)" v-for="(subitem, subindex) in item.goodsList" :key="subindex">
+          <div >
             <img :src="subitem.list_pic_url" alt="">
             <p>{{subitem.name}}</p>
-            <p><span class="realPrice">￥{{subitem.retail_price}}</span><span class="virtualPrice">￥{{subitem.retail_price}}</span><span class="purchaseImmediately">立即购买</span></p>
-          </div>
-          <div @click="categoryList(item.id)">
-            <div class="last">
-              <p>{{item.name}}好物</p>
-              <span class="icon"></span>
-            </div>
+            <p><span class="realPrice">￥{{subitem.retail_price}}</span><span class="virtualPrice">￥{{subitem.retail_price}}</span><span class="purchaseImmediately" @click="goodsDetail(subitem.id)">立即购买</span></p>
           </div>
         </div>
       </div>
@@ -142,21 +44,16 @@
 </template>
 
 <script>
-import amapFile from "../../utils/amap-wx";
-// import { get } from "../../utils";
-import { mapState, mapMutations } from "vuex";
 export default {
   onLoad() {
-    // this.getCityName();
+
   },
   onShow() {
-    if (wx.getStorageSync("cityName")) {
-      this.cityName = wx.getStorageSync("cityName");
-      wx.removeStorageSync("cityName");
-    }
+
+
   },
   computed: {
-    // ...mapState(["cityName"])
+
   },
   mounted() {
     this.getData();
@@ -178,60 +75,8 @@ export default {
   },
   components: {},
   methods: {
-    // ...mapMutations(["update"]),
-    toMappage() {
-      var _this = this;
-      // 可以通过 wx.getSetting 先查询一下用户是否授权了 "scope.record" 这个 scope
-      wx.getSetting({
-        success(res) {
-          //如果没有同意授权,打开设置
-          if (!res.authSetting["scope.userLocation"]) {
-            wx.openSetting({
-              success: res => {
-                console.log(2222)
-                _this.getCityName();
-              }
-            });
-          } else {
-            console.log(1111)
-            wx.navigateTo({
-              url: "/pages/mappage/main"
-            });
-          }
-        }
-      });
-    },
-    getCityName() {
-      var _this = this;
-      var myAmapFun = new amapFile.AMapWX({
-        key: "e545e7f79a643f23aef187add14e4548"
-      });
-      myAmapFun.getRegeo({
-        success: function(data) {
-          //成功回调
-          console.log(data);
-          // data[0].regeocodeData.formatted_address
-          _this.cityName = data[0].regeocodeData.formatted_address;
-          _this.update({ cityName: data[0].regeocodeData.formatted_address });
-        },
-        fail: function(info) {
-          //失败回调
-          console.log(info);
-          //如果用户拒绝授权
-          // 默认为北京
-          _this.cityName = "北京市";
-          // _this.update({ cityName: "北京市" });
-        }
-      });
-    },
-    toSearch() {
-      wx.navigateTo({
-        url: "/pages/search/main"
-      });
-    },
     async getData() {
       // const data = await get("/index/index");
-      // 埋点
       var that =  this;
       wx.request({
         url: "https://www.heyuhsuo.xyz/heyushuo/index/index",
@@ -241,12 +86,31 @@ export default {
           console.log(res)
           if (res.data.success) {
             that.banner = res.data.banner;
-            that.channel = res.data.channel;
+            // that.channel = res.data.channel;
             that.brandList = res.data.brandList;
             that.newGoods = res.data.newGoods;
             that.hotGoods = res.data.hotGoods;
             that.topicList = res.data.topicList;
-            that.newCategoryList = res.data.newCategoryList;
+            that.newCategoryList = res.data.newCategoryList[0].goodsList;
+            // that.channel[0].flag = true;
+            wx.request({
+              url: "https://www.heyuhsuo.xyz/heyushuo/category/categoryNav?id="+ res.data.channel[0].id,
+              method: "get",
+              header: {'content-type': 'application/x-www-form-urlencoded'},
+              success: function (res) {
+                console.log(res)
+                if (res.statusCode == 200) {
+                  that.channel = res.data.navData;
+                  that.channel[0].flag = true;
+                }else{
+                  wx.showToast({
+                    title: res.data.msg,
+                    icon: 'none',
+                    duration: 2000
+                  })
+                }
+              }
+            })
           }else{
             wx.showToast({
               title: res.data.msg,
@@ -263,41 +127,33 @@ export default {
         url: "/pages/goods/main?id=" + id
       });
     },
-    categoryList(id) {
-      wx.navigateTo({
-        url: "/pages/categorylist/main?id=" + id
-      });
-    },
-    goodsList(info) {
-      if (info == "hot") {
-        wx.navigateTo({
-          url: "/pages/newgoods/main?isHot=" + 1
-        });
-      } else {
-        wx.navigateTo({
-          url: "/pages/newgoods/main?isNew=" + 1
-        });
+    categoryList(item,index) {
+      var that = this;
+      for(var i=0;i<that.channel.length;i++){
+        if(i== index){
+          that.channel[i].flag = true;
+        }else{
+          that.channel[i].flag = false;
+        }
       }
-    },
-    topicdetail(id) {
-      wx.navigateTo({
-        url: "/pages/topicdetail/main?id=" + id
-      });
-    },
-    totopic() {
-      wx.navigateTo({
-        url: "/pages/topic/main"
-      });
-    },
-    tobrandList() {
-      wx.navigateTo({
-        url: "/pages/brandlist/main"
-      });
-    },
-    branddetail(id) {
-      wx.navigateTo({
-        url: "/pages/branddetail/main?id=" + id
-      });
+      wx.request({
+        url: "https://www.heyuhsuo.xyz/heyushuo/goods/goodsList?categoryId="+item.id,
+        method: "get",
+        // data:{categoryId: item.id},
+        header: {'content-type': 'application/x-www-form-urlencoded'},
+        success: function (res) {
+          console.log(res)
+          if (res.statusCode == 200 ) {
+            that.newCategoryList = res.data.data;
+          }else{
+            wx.showToast({
+              title: res.data.msg,
+              icon: 'none',
+              duration: 2000
+            })
+          }
+        }
+      })
     }
   },
   created() {}
