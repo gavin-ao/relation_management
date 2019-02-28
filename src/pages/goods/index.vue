0b43cbea4686da2134d4c73fd5cbec0c182dcbe0
liuxiaolei319 <182150305@qq.com>
2019-02-28 15:11:58 +0800

    1.增加通过分享进入到商品详情页时，如果新用户需要授权同。  lxl 2019.02.28

--- src/pages/goods/index.vue
+++ src/pages/goods/index.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="goods">
+    <div class="authorizes" v-if="helpInfos==true">
+      <div class="logo">
+        <img src="/static/images/logo.png" alt="">
+        <p style="color: #000;font-size: 18px;">分销e</p>
+        <p>购物商城</p>
+      </div>
+      <p style="border-top:1px solid #ccc;width: 100%;margin: 60px 0 0 0;"></p>
+      <div class="info">
+        <p>该程序将获得以下授权</p>
+        <p><span class="dot">·</span><span>获得您的公开信息（头像、昵称等）</span></p>
+        <p>(取消授权您可能错过很多功能哦～)</p>
+      </div>
+      <div class="btn">
+        <div>
+          <button open-type="getUserInfo" @getuserinfo="helpInfoss" class="save-btn">立即授权</button>
+        </div>
+      </div>
+    </div>
+    <block v-if="tempFlag==true">
     <div class="swiper">
       <swiper class="swiper-container" autoplay="true" interval="3000" duration="1000">
         <block v-for="(item, index) in gallery " :key="index">
@@ -86,28 +105,79 @@
       </div>
       <div class="purchaseImmediately" @click="bug">立即购买</div>
     </div>
+    </block>
   </div>
 </template>
 
 <script>
   // import { get, post, toLogin, login, getStorageOpenid } from "../../utils";
   import wxParse from "mpvue-wxparse";
+  import utils from "../../utils/utils.js";;
 
   export default {
-    onLoad() {
-      this.initData();
+    onLoad(option) {
+        console.log("id: "+option.id)
+        this.goodId = option.id;
+        console.log(this.goodId)
+        // this.paths = that.$root.$mp.query.paths;
+        if(option.myInvitation){
+          this.myInvitation = option.myInvitation
+        }
       //判断是否登录获取用户信息
       // if (login()) {
       //   this.userInfo = login();
       // }
-      this.id = this.$root.$mp.query.id;
-      this.paths = this.$root.$mp.query.paths;
-      console.log(this.id)
-      this.goodsDetail();
+      // this.id = this.$root.$mp.query.id;
+
+
       wx.setNavigationBarTitle({
         title: "商品详情页"
       })
     },
+    onShow(option){
+      console.log("option: "+option)
+      var that = this;
+      // that.id = that.id;
+      // that.paths = that.$root.$mp.query.paths;
+      console.log("商品id："+that.goodId)
+
+      if(that.myInvitation != ""){
+        console.log("邀请号："+that.myInvitation)
+        that.$store.state.board.otherInvitation = that.myInvitation
+      }else{
+        that.$store.state.board.otherInvitation=''
+      }
+
+      that.initData();
+      wx.getSetting({
+        success: (res) => {
+          console.log(res);
+          if (res.authSetting['scope.userInfo']) {
+            // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+            wx.showLoading({
+              title: '加载中',
+            })
+            utils.login(that, false, function (sessionId) {
+              console.log(333333)
+              that.goodsDetail();
+              // wx.redirectTo({
+              //   // url: '/pages/addaddress/main'
+              //   url: '/pages/addressSelect/main'
+              // })
+            })
+            that.helpInfos = false;
+          }
+          else {
+            // wx.redirectTo({
+            //   url: '/pages/authorize/main'
+            // })
+            that.tempFlag = false;
+            that.helpInfos = true;
+          }
+        }
+      })
+    },
+
     // //商品转发
     // onShareAppMessage() {
     //   console.log(this.info.name);
@@ -137,19 +207,47 @@
         productList: [],
         goods_desc: [],
         id: "",
+        goodId:"",
         paths: "",
         userInfo: "",
         goodsId: "",
         allPrise: "",
         currentNum: 0,
         scrollTop: 0,
-        salesVolume:0
+        salesVolume:0,
+        myInvitation:'',
+        helpInfos: false,
+        tempFlag: false
       };
     },
     components: {
       wxParse
     },
     methods: {
+      helpInfoss() {
+        var that = this;
+        console.log("授权成功");
+        utils.login(that,false, function (sessionID) {
+          that.helpInfos = false;
+          console.log("ddd： " + that.$store.state.board.otherInvitation)
+          if (that.$store.state.board.otherInvitation) {
+            console.log("授权成功")
+            that.goodsDetail();
+          }
+        });
+        // that.utilss();
+      },
+      // utilss() {
+      //   var that = this;
+      //   utils.login(that, function (sessionID) {
+      //     that.helpInfos = false;
+      //     console.log("ddd： "+that.$store.state.board.otherInvitation)
+      //     if (that.$store.state.board.otherInvitation) {
+      //       console.log("授权成功")
+      //       that.goodsDetail();
+      //     }
+      //   });
+      // },
       initData() {
         this.gallery = [];
         this.info = {};
@@ -197,7 +295,7 @@
             if (res.data.success) {
               _this.$store.state.board.myInvitation= res.data.invitation;
               wx.navigateTo({
-                url: "/pages/showPages/main"
+                url: "/pages/showPages/main?commodityId="+_this.info.commodityId
               });
             } else {
               wx.showToast({
@@ -212,20 +310,24 @@
       },
       async goodsDetail() {
         var that = this;
+        that.tempFlag = true;
+        console.log("sessionID："+that.$store.state.board.sessionID)
+        console.log("id："+that.goodId)
         wx.request({
           url: that.$store.state.board.urlHttp + '/wechatapi/commodity/getCommodityById',
           method: "post",
-          data:{commodityId: that.id,sessionID:that.$store.state.board.sessionID},
+          data:{commodityId: that.goodId,sessionID:that.$store.state.board.sessionID},
           // data: {commodityId: 3, sessionID: that.$store.state.board.sessionID},
           header: {'content-type': 'application/x-www-form-urlencoded'},
           success: function (res) {
+            console.log("返回的信息")
             console.log(res)
             var data = res.data;
             if (data.success) {
               that.salesVolume = data.salesVolume;
               that.$store.state.board.share = data.share;
-              that.gallery.push(that.paths);
-              that.$store.state.board.goToLink = that.paths;
+              that.gallery.push( that.$store.state.board.urlHttp +"/static/file/"+data.commodityVO.filePath);
+              that.$store.state.board.goToLink = that.$store.state.board.urlHttp +"/static/file/"+data.commodityVO.filePath;
               if ( that.$store.state.board.goToLink) {
                 wx.getImageInfo({
                   src:  that.$store.state.board.goToLink,
@@ -261,4 +363,70 @@
 <style lang='scss' scoped>
   @import url("~mpvue-wxparse/src/wxParse.css");
   @import "./style.scss";
+  .authorizes {
+    margin: 0 auto;
+    font-size: 14px;
+    color: #ccc;
+    .logo {
+      img {
+        display: block;
+        width: 70px;
+        height: 70px;
+        margin: 0 auto;
+      }
+      p {
+        text-align: center;
+        margin-top: 5px;
+      }
+    }
+    .info {
+      width: 100%;
+      font-size: 12px;
+      padding: 10px;
+      box-sizing: border-box;
+      p {
+        line-height: 24px;
+        span {
+          display: inline-block;
+          vertical-align: middle;
+        }
+        .dot {
+          /*font-size: 12px;*/
+          font-weight: bold;
+          padding-right: 5px;
+        }
+      }
+    }
+    .btn {
+      width: 90%;
+      margin: 0 auto;
+      margin-top: 60px;
+      div {
+        width: 100%;
+        display: inline-block;
+        text-align: center;
+        button {
+          font-size: 14px;
+          font-family: "Microsoft YaHei UI Light";
+          border: 0;
+          margin: 0 auto;
+        }
+        .save-btn {
+          width: 100%;
+          height: 40px;
+          line-height: 40px;
+          text-align: center;
+          margin-top: 15px;
+          border-radius: 3px;
+          box-sizing: border-box;
+          background-color:  #0096D6;
+          color: #fff;
+        }
+        button[type="default"] {
+          background-color: #f1f1f1;
+          color: #000;
+        }
+      }
+    }
+  }
 </style>
