09cd7cea24c0ba1572360b9aa1b6f428b5ad2055
liuxiaolei319 <182150305@qq.com>
2019-03-01 09:52:56 +0800

    增加用户授权 lxl 2019.02.28

--- src/pages/logs/index.vue
+++ src/pages/logs/index.vue
@@ -5,19 +5,43 @@
 </template>
 
 <script>
+  import utils from "../../utils/utils.js";
+  export default {
 
-export default {
+    data() {
+      return {
+        logs: []
+      }
+    },
+    onLoad() {
+      var that = this;
+      wx.getSetting({
+        success: (res) => {
+          console.log(res);
+          if (res.authSetting['scope.userInfo']) {
+            // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+            wx.showLoading({
+              title: '加载中',
+            })
+            utils.login(that, false, function (sessionId) {
+              console.log(333333)
+              wx.switchTab({
+                url: '/pages/index/main'
+              })
+            })
+          }
+          else {
+            wx.redirectTo({
+              url: '/pages/authorize/main'
+            })
+          }
+        }
+      })
+    },
+    created() {
 
-  data () {
-    return {
-      logs: []
     }
-  },
-
-  created () {
-
   }
-}
 </script>
 
 <style>
